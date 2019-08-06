fedd87225a326f9374ee5124f999384639cc79bd
Beenish-Syed <bsyed@csumb.edu>
2019-08-06 20:54:59 +0000

    adding get order by order Id

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     #get('status' => 'status#index')
     #resources :customers
     post '/orders' => 'orders#create'
-    get '/orders/:id' => 'orders#getOrderByCustId'
+    get '/orders/:id' => 'orders#getOrderById'
     
     #root :to => redirect('/customers')
     #For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
