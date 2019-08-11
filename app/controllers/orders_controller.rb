15ba276a8cdfc3d646d356a971e2fb28f3c47229
EC2 Default User <ec2-user@ip-172-31-27-163.us-west-2.compute.internal>
2019-08-11 23:24:37 +0000

    RSPEC Updates

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,143 +1,131 @@
 class OrdersController < ApplicationController
-     #POST /orders
+    #POST request.
     def create
-        #create a new order
+        p params, "This was the params"
         @order = Order.new
-        
-        #retrieving the customer information from the customer app
         code, customer = Customer.getCustomerByEmail(params[:email])
         
-        if code != 200 #customer doesn't exist
-            render(json: @order, status: 400) #give error message
-            return #return as customer doesnt exit order can't be created
+        if code != 200
+            render(json: @order, status: 400)
+            p "THIS is the code ", code
+            return
         end
+        p customer['id']
+
         
-        #retrieving the item information from item  app
         code, item = Item.getItemById(params[:itemId])
         
-        if code != 200 #the item doesn't exist
+        if code != 200
             render(json: @order, status: 400)
             return
         end
-        #checking if the stock qty is 0 the order can't be processed
+
         if item['stockQty'] == 0
             render(json: @order, status: 400)
             return
         end
-        #processing and creating the order
-        @order.customerId = customer[0]["id"]
-        @order.award = customer[0]["award"]  
-        @order.description = item['description']
+        
+        
+        @order.customerId = customer['id']
+        @order.award = customer['award']        
+        @order.description = item['desciption']
         @order.itemId = item['id']
-        @order.price = item['price'] 
-        if customer[0]['award']==nil
-            @order.total = item['price']
+        @order.price = item['price']        
+        @order.total = item['price'] - customer['award']
+        Customer.putOrder(@order)
+        Item.putOrder(@order)
+        render(json: @order, status: 201)
+=begin
+        @customer = get 'customers'
+        p "THIS is a TEST", @customer
+        if @customer.save
+            data = {:id=> @customer.id, :email=> @customer.email, :firstName =>@customer.firstName, :lastName => @customer.lastName,
+                    :lastOrder=> @customer.lastOrder, :lastOrder2=> @customer.lastOrder2, :lastOrder3 => @customer.lastOrder3, :award => @customer.award
+            }
+            render(json: data , status: 201 )
         else
-            @order.total = item['price'] - customer[0]['award']
-        end
-        if @order.save
-            itemHash = Hash.new
-            itemHash ={id: @order.itemId}
-            Item.putOrder(itemHash)
-            orderHash = Hash.new
-            orderHash = {custId: @order.customerId, total: item['price']}
-            Customer.updateCustomerOrder(orderHash)
-            render(json: @order, status: 201)
-        else
-            render(json: {mesages: @order.errors.messages}, status: 400)
-        end
-    end
+            render(json: @customer.errors, status: 400 )
+=end        end
 
-    #GET/orders by order id customer id or customer email
-    def getOrder
-        if request.query_string.present? 
-            if params[:id].present?
-                @order=Order.find_by(id: params[:id])
-                if @order.nil?
-                    head 404 #error - not found
-                else
-                    render "order.json.jbuilder"
-                end
-            elsif params[:customerId].present?
-                @order = Order.where(:customerId => params[:customerId])
-                #@order is an ActiveRecord::Relation @order.where returns an ActiveRecord::Relation
-                #converting @order to an array of Order objects
-                @order=@order.to_a
-                if @order.nil?
-                    head 404 #error - not found
-                else
-                    render "abc.json.jbuilder"
-                end 
-            elsif params[:email].present?
-                #retrieving the customer information from the customer app
-                @customer = Customer.getCustomerByEmail(params[:email]) #customer is the response from the method
-                                                                    # in customer helper class
-                id=@customer["id"]
-                #find the order using the customer Id
-                @order = Order.where(:customerId => id)
-                @order.to_a
-                if @order.nil?
-                    head 404 #error - not found
-                else
-                    render "abc.json.jbuilder"
-                end 
-            end    
-        end
     end
+    def order_params
+        params.require(:order).permit(:itemId, :email)
 
-    #RegisterCustomer
-    def regCustomer
-        customerHash = {email: params[:email], firstName: params[:firstName], lastName: params[:lastName]}
-        @customer = Customer.createCustomer(customerHash) #call the helper class Customer method
-        render(json: @customer, status: 200)
     end
-    
-    #get customer by email or get customer by id
-    def getCustomer
-        if request.query_string.present? 
-            if params[:email].present?
-                @customer = Customer.getCustomerByEmail(params[:email]) #Calling the helper Customer class method
-                if @customer.nil?
-                    head 404 #error - not found
-                else
-                    render(json: @customer, status: 200)
-                end
-            elsif params[:id].present?
-                @customer = Customer.getCustomerById(params[:id]) #Calling the helper Customer class method
-                if @customer.nil?
-                    head 404 #error - not found
-                else
-                    render(json: @customer, status: 200)
-                end
-            end
-        else
-            head 404
-        end
-    end
-    
-    #create Item
-    def createItem
-        itemHash = Hash.new
-        itemHash = {id: params[:id], description: params[:description], price: params[:price], stockQty: params[:stockQty]}
-        @item = Item.createItem(itemHash)
-        if @item.nil?
-            head 404 #error - not found
-        else
-            render(json: @item, status: 200)
-        end
-    end
-    
-    #get item by Id
-    def getItemById
-        if params[:id].present?
-            @item = Item.getItemById(params[:id])
-            if @item.nil?
-                head 404 #error - not found
+    #GET request
+=begin    def index
+        if params['email'].present?
+            @customer = Customer.where(:email => params['email'])
+            if @customer.length>0
+                render(json: @customer , status: 200 )
+            else #Test this to see if needed.
+               render(json: @customer, status: 404)
+            end 
+        elsif params['id'].present?
+            @customer = Customer.where(:id => params['id'])
+            if @customer.length>0
+                render(json: @customer , status: 200 )
             else
-                render(json: @item, status: 200)
-            end
+               render(json: @customer, status: 404)
+            end 
         else
-            head 404 #error - not found
+           render(head :statuscode, status: 404 )
         end
     end
-end
+    
+    def update
+        if @customer.length > 0
+            if params['award'] == 0
+                if @customer.pluck(:lastOrder)[0] == nil
+                    @customer.update(:lastOrder => params['total'])
+                    render(json: @customer , status: 204 )
+                #elsif @customer.update(customer_params)
+                    #render(json: @customer , status: 204 )
+                elsif @customer.pluck(:lastOrder2)[0] == nil
+                    @customer.update(:lastOrder2 => params['total'])
+                    render(json: @customer , status: 204 )
+                elsif @customer.pluck(:lastOrder3)[0] == nil
+                    @customer.update(:lastOrder3 => params['total'])
+                    award = (@customer.pluck(:lastOrder)[0] + @customer.pluck(:lastOrder2)[0] + @customer.pluck(:lastOrder3)[0])/30  
+                    
+                    @customer.update(:award => award)
+                    render(json: @customer , status: 204 )
+                else
+                   # render(json: @customer, status: 400 )
+                end
+            else
+                @customer.update(:lastOrder => 0)
+                @customer.update(:lastOrder2 => 0)
+                @customer.update(:lastOrder3 => 0)
+                @customer.update(:award => 0)
+                render(json: @customer , status: 204 )
+            end
+        else
+            render(json: @customer, status: 400 )
+        end
+            
+    end
+    
+    def customer_params
+        params.require(:customer).permit(:email, :firstName, :lastName)
+
+    end
+    def set_customer
+
+      @customer = Customer.where(:id => params['customerId'])
+
+    end
+    def new
+        @customer = Customer.new
+=end    end
+end   
+
+=begin                data = {:id => @customer.pluck(:id)[0], 
+                        :email => @customer.pluck(:email)[0], 
+                        :firstName => @customer.pluck(:firstName)[0], 
+                        :lastName => @customer.pluck(:lastName)[0], 
+                        :lastOrder => @customer.pluck(:lastOrder)[0], 
+                        :lastOrder2 => @customer.pluck(:lastOrder2)[0], 
+                        :lastOrder3 => @customer.pluck(:lastOrder3)[0], 
+                        :award => @customer.pluck(:award)[0], 
+=end                }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-    post '/orders' => '#create'
+    #post '/orders' => '#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     
     post '/orders' => 'orders#create'
