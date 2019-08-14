fe08bdb3fa568e3e9bd18a084daaa3894d2b2c6c
EC2 Default User <ec2-user@ip-172-31-27-163.us-west-2.compute.internal>
2019-08-14 03:18:05 +0000

    Removed unnecessary comments. Fixed error where get order by email used an incorrect email address

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,27 +6,20 @@ class OrdersController < ApplicationController
         
         #retrieving the customer information from the customer app
         code, @customer = Customer.getCustomerByEmail(params[:email])
-        #response = Customer.getCustomerByEmail(params[:email])
-        
-        #@customer = JSON.parse response.body
-        #p response, "This was the response"
-        #if code != 200 #customer doesn't exist
+
         if code !=200
-           # render(json: @order, status: 400) #give error message
             render(json: {messages: 'Customer email you entered does not exist'}, status: 400)
             return #return as customer doesnt exit order can't be created
         end
         
         #retrieving the item information from item  app
         code, @item = Item.getItemById(params[:itemId])
-        #@item = Item.getItemById(params[:itemId])
-        #response = Item.getItemById(params[:itemId])
+
         if code != 200 #the item doesn't exist
             render(json: {messages: 'Item id you entered does not exist'}, status: 400)
-            #render(json: @order, status: 400)
             return
         end
-        #@item = JSON.parse response.body
+
         #checking if the stock qty is 0 the order can't be processed
         if @item['stockQty'] == 0
             render(json: @order, status: 400)
@@ -64,7 +57,7 @@ class OrdersController < ApplicationController
                 @order=Order.find_by(id: params[:id])
                 if @order.nil?
                     render(json: {messages: 'Order for the order Id not found'}, status: 404)
-                    #head 404 #error - not found
+
                 else
                     render "order.json.jbuilder"
                 end
@@ -72,10 +65,8 @@ class OrdersController < ApplicationController
                 @order = Order.where(:customerId => params[:customerId])
                 #@order is an ActiveRecord::Relation @order.where returns an ActiveRecord::Relation
                 #converting @order to an array of Order objects
-               # @order=@order.to_a
-                #if @order.nil?
+
                 if @order.empty?
-                    #head 404 #error - not found
                     render(json: {messages: 'Order for customer Id not found'}, status: 404)
                 else
                     @order=@order.to_a
@@ -85,6 +76,10 @@ class OrdersController < ApplicationController
                 #retrieving the customer information from the customer app
                 code, @customer = Customer.getCustomerByEmail(params[:email]) #customer is the response from the method
                                                                     # in customer helper class
+                if code != 200 #the item doesn't exist
+                    render(json: {messages: 'Customer email does not exist'}, status: 400)
+                    return
+                end            
                 id=@customer["id"]
                 p id, "This is the customer ID"
                 #find the order using the customer Id
@@ -116,7 +111,6 @@ class OrdersController < ApplicationController
                 code, @customer = Customer.getCustomerByEmail(params[:email]) #Calling the helper Customer class method 
                                                                         #which sends back a response
                 if code == 404
-                    #head 404 #error - not found
                     render(json: {messages: 'Customer email not found'}, status: 404)
                 else
                     render(json: @customer, status: 200)
@@ -125,7 +119,6 @@ class OrdersController < ApplicationController
                 response = Customer.getCustomerById(params[:id]) #Calling the helper Customer class method
                 if response.code == 404
                     render(json: {messages: 'Customer id not found'}, status: 404)
-                    #head 404 #error - not found
                 else
                     @customer = JSON.parse response.body
                     render(json: @customer, status: 200)
@@ -153,15 +146,12 @@ class OrdersController < ApplicationController
         if params[:id].present?
             code, @item = Item.getItemById(params[:id])
             if response.code == 404
-                #head 404 #error - not found
                 render(json: {messages: 'Item not found'}, status: code)
             else
                
                 render(json: @item, status: 200)
             end
-        #else
-        #    #head 404 #error - not found
-        #    render(json: {messages: 'Item not found'}, status: 404)
+
         end
     end
 end
--- spec/requests/orders_spec.rb
+++ spec/requests/orders_spec.rb
@@ -27,7 +27,6 @@ RSpec.describe "Orders", type: :request do
                 expect(order[:id]).to eq 227
                 201
             end 
-            p order.to_json
             post '/orders', params: order.to_json, headers: headers
 
         end
@@ -57,7 +56,6 @@ RSpec.describe "Orders", type: :request do
             expect(order[:id]).to eq 100
             201
             end 
-            p order.to_json
             
             post '/orders', params: order.to_json, headers: headers
             
@@ -79,8 +77,7 @@ RSpec.describe "Orders", type: :request do
             expect(orderdb['customerId']).to eq 1
             expect(orderdb['price']).to eq 175.00
             expect(orderdb['description']).to eq "jewelry item"
-            
-          #order_json = JSON.parse(response)
+
         end
         it "customer makes purchase and an award is applied to total" do
             order = { itemId: 100, 
@@ -107,7 +104,6 @@ RSpec.describe "Orders", type: :request do
             expect(order[:id]).to eq 100
             201
             end 
-            p order.to_json
             
             post '/orders', params: order.to_json, headers: headers
             
@@ -130,8 +126,7 @@ RSpec.describe "Orders", type: :request do
             expect(orderdb['price']).to eq 175.00
             expect(orderdb['total']).to eq 75.00
             expect(orderdb['description']).to eq "jewelry item"
-            
-          #order_json = JSON.parse(response)
+
         end        
         it "Invalid customer attempts to make a purchase" do
           order = { itemId: 100, 
@@ -215,7 +210,6 @@ RSpec.describe "Orders", type: :request do
         it "Access order by customer ID" do
             get '/orders?customerId=2', headers: @headers
             order = JSON.parse(response.body)[0]
-            p order, "This is the order"
             expect(response).to have_http_status(200)
             expect(order.keys).to contain_exactly(
                 'id',
@@ -246,7 +240,7 @@ RSpec.describe "Orders", type: :request do
             end 
             get '/orders?email=xy@csumb.edu', headers: @headers
             order = JSON.parse(response.body)[0]
-            p order, "This is the order"
+
             expect(response).to have_http_status(200)
             expect(order.keys).to contain_exactly(
                 'id',
@@ -273,236 +267,6 @@ RSpec.describe "Orders", type: :request do
             get '/orders?email=test1234@email.com', header: @headers
             expect(response).to have_http_status(404)
         end
-#
-        #it "Access order by order ID"
-    end
-=begin    before(:each) do 
-        # create database record for Test Dummy
-        #Order.create(itemId: "5", customerId:"2")
-        
-    end
-    
-    #Create a new customer
-    describe "POST /orders" do
-        it 'post of new order should return customer information' do
-            #headers = { "CONTENT_TYPE" => "application/json" ,
-           #        "ACCEPT" => "application/json"}    # Rails 4
-            order_new = {itemId: 1, email:"wb@csumb.edu"}
-            testItem = double('item')
-            allow(testItem).to receive(:itemId) {2}
-            allow(testItem).to receive(:description) {"this is a test"}
-            allow(testItem).to receive(:price) {100}
-            
-            testCustomer = double('Customer')
-            allow(testCustomer).to receive(:customerId){2}
-            allow(testCustomer).to receive(:award){2}
-            
-            post '/orders', params: order_new.to_json, headers: @headers
-            expect(response).to have_http_status(201)
-            
-            #Make sure the JSON response contains the correct fields
-            order = JSON.parse(response.body)
-            expect(order.keys).to contain_exactly(
-                'id',
-                'itemId',
-                'description',
-                'customerId',
-                'price',
-                'award',
-                'total',
-                'created_at',
-                'updated_at')
-            
-           
-            #expect(customer['email']).to eq 'williamdotbaker@gmail.com' #Look at screenshot
-            #expect(customer['firstName']).to eq 'William'
-            #expect(customer['lastName']).to eq 'Baker'
-            
-            #Check to make sure record was recorded to database
-            #customerdb = Customer.find(customer['id'])
-            #expect(customerdb['email']).to eq 'williamdotbaker@gmail.com' #look at screenshot
-            #expect(customerdb['firstName']).to eq 'William'
-            #expect(customerdb['lastName']).to eq 'Baker'
-        end
-    end
-=begin        it 'post of an invalid customer will return an error' do
-            customer_new = {email: '', firstName: '', lastName: ''}
-            post '/customers', params: customer_new.to_json, headers: headers
-            expect(response).to have_http_status(400)
-            
-            customer_new = {email: 'williamdotbaker@gmail.com', firstName: '', lastName: ''}
-            post '/customers', params: customer_new.to_json, headers: headers
-            expect(response).to have_http_status(400)
-            
-            
-            customer_new = {email: '', firstName: 'William', lastName: 'Baker'}
-            post '/customers', params: customer_new.to_json, headers: headers
-            expect(response).to have_http_status(400)
-        end
-        it 'post of a duplicate customer will return an error' do
-            customer_new = {email: 'testdum@yahoo.com', firstName: 'Test', lastName: 'Dummy'}
-            post '/customers', params: customer_new.to_json, headers: headers
-            expect(response).to have_http_status(400)
-        end
-    end
-    
-    describe "GET /customers?email" do
-        it 'get customer information for me should return JSON array of length 1 of customer objects' do
-            
-            headers = { "ACCEPT" => "application/json"}    # Rails 4 
-            get '/customers?email=testdum@yahoo.com', headers: headers
-            json_response = JSON.parse(response.body)
-            expect(response).to have_http_status(200)    
-            expect(json_response.length).to eq 1
-            customer = json_response[0]
-            expect(customer.keys).to contain_exactly(
-                'id',
-                'email',
-                'firstName',
-                'lastName',
-                'lastOrder',
-                'lastOrder2',
-                'lastOrder3',
-                'award'
-                )
-            expect(customer['email']).to eq 'testdum@yahoo.com'
-            expect(customer['firstName']).to eq 'Test'
-            expect(customer['lastName']).to eq 'Dummy'
-        end
-        it 'get customer information for undefined customer should return JSON array of length 0 and 404 error' do
-            headers = { "ACCEPT" => "application/json"}    # Rails 4 
-            get '/customers?email=unreal@yahoo.com', headers: headers
-            json_response = JSON.parse(response.body)
-            expect(json_response.length).to eq 0
-            expect(response).to have_http_status(404)  
-        end
-    end
-    describe "GET /customers?id" do
-        it 'get customer information for me should return JSON array of length 1 of customer objects' do
-            
-            headers = { "CONTENT_TYPE" => "application/json" , "ACCEPT" => "application/json"}    # Rails 4 
-            get '/customers?id=1', headers: headers
-            json_response = JSON.parse(response.body)
-            expect(json_response.length).to eq 1
-            customer = json_response[0]
-            expect(customer.keys).to contain_exactly(
-                'id',
-                'email',
-                'firstName',
-                'lastName',
-                'lastOrder',
-                'lastOrder2',
-                'lastOrder3',
-                'award'
-                )
-            expect(customer['id']).to eq 1
-            expect(customer['email']).to eq 'testdum@yahoo.com'
-            expect(customer['firstName']).to eq 'Test'
-            expect(customer['lastName']).to eq 'Dummy'
-            expect(response).to have_http_status(200) 
-        end
-        it 'get customer information for undefined customer should return JSON array of length 0 and 404 error' do
-            headers = { "ACCEPT" => "application/json"}    # Rails 4 
-            get '/customers?id=1000', headers: headers
-            json_response = JSON.parse(response.body)
-            expect(json_response.length).to eq 0
-            expect(response).to have_http_status(404) 
-        end
-    end
-    
-    describe "PUT /customers/order" do
-        it 'post of new customer should return customer information' do
-            headers = { "CONTENT_TYPE" => "application/json" ,
-                   "ACCEPT" => "application/json"}    # Rails 4
-            new_customer = {email: 'williamdotbaker@gmail.com', firstName: 'William', lastName: 'Baker'}
-            post '/customers', params: new_customer.to_json, headers: headers
-            expect(response).to have_http_status(201)
-            
-            customer = JSON.parse(response.body)
-            expect(customer['lastOrder']).to eq nil
-            #Create the double for the order class
-            
-            order1 = double('order')
-            allow(order1).to receive(:parameters) {{id: 1, itemId: 123, customerId: 2, price: 200, award: 0, total: 200}}
 
-            put "/customers/order", params: order1.parameters.to_json, headers: headers
-            #Make sure the JSON response contains the correct fields
-
-            #customer2 = JSON.parse(response.body)
-            expect(response).to have_http_status(204)
-            #expect(customer2['lastOrder']).to eq 200
-            customerdb = Customer.find(customer['id'])
-            expect(customerdb['lastOrder']).to eq 200            
-            
-        end
-        
-        it 'does not update invalid customer and returns error' do
-            headers = { "CONTENT_TYPE" => "application/json" ,
-                   "ACCEPT" => "application/json"}    # Rails 4
-            new_customer = {email: 'williamdotbaker@gmail.com', firstName: 'William', lastName: 'Baker'}
-            post '/customers', params: new_customer.to_json, headers: headers
-            expect(response).to have_http_status(201)
-            
-            customer = JSON.parse(response.body)
-            
-            #Create the double for the order class
-            
-            order1 = double('order')
-            allow(order1).to receive(:parameters) {{id: 1, itemId: 123, customerId: 800, price: 200, award: 0, total: 200}}
-
-            put "/customers/order", params: order1.parameters.to_json, headers: headers
-            #Make sure the JSON response contains the correct fields
-            expect(response).to have_http_status(400)
-            
-            customerdb = Customer.find(customer['id'])
-            expect(customerdb['lastOrder']).to eq customer['lastOrder']            
-            
-        end
     end
-    
-    describe "Customer makes 4 consecutive purchases with an award applied on the last one" do
-        it "An award is applied on the last order" do
-            headers = { "CONTENT_TYPE" => "application/json" ,
-                   "ACCEPT" => "application/json"}    # Rails 4
-            new_customer = {email: 'williamdotbaker@gmail.com', firstName: 'William', lastName: 'Baker'}
-            post '/customers', params: new_customer.to_json, headers: headers
-            expect(response).to have_http_status(201)
-            
-            customer = JSON.parse(response.body)
-            expect(customer['lastOrder']).to eq nil
-            
-            #Create the double for the order class and the first 3 order objects
-            order1 = double('order')
-            allow(order1).to receive(:parameters) {{id: 1, itemId: 123, customerId: 2, price: 200, award: 0, total: 200}}
-
-            order2 = double('order')
-            allow(order2).to receive(:parameters) {{id: 2, itemId: 777, customerId: 2, price: 600, award: 0, total: 600}}
-        
-            order3 = double('order')
-            allow(order3).to receive(:parameters) {{id: 3, itemId: 321, customerId: 2, price: 100, award: 0, total: 100}}
-            
-            #Begin putting in the orders and test to ensure that 
-            put "/customers/order", params: order1.parameters.to_json, headers: headers
-            put "/customers/order", params: order2.parameters.to_json, headers: headers
-            put "/customers/order", params: order3.parameters.to_json, headers: headers
-            
-            customerdb = Customer.find(customer['id'])
-            
-            expect(customerdb['lastOrder']).to eq 200
-            expect(customerdb['lastOrder2']).to eq 600
-            expect(customerdb['lastOrder3']).to eq 100
-            
-            expect(customerdb['award']).to eq 30
-            
-            order4 = double('order')
-            allow(order4).to receive(:parameters) {{id: 4, itemId: 421, customerId: 2, price: 250, award: 30, total: 220}}
-            put "/customers/order", params: order4.parameters.to_json, headers: headers
-            
-            customerdb = Customer.find(customer['id'])
-            expect(customerdb['lastOrder']).to eq 0
-            expect(customerdb['lastOrder2']).to eq 0
-            expect(customerdb['lastOrder3']).to eq 0
-            expect(customerdb['award']).to eq 0
-        end
-=end    end
 end
